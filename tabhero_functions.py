8971dd3bea0ef571b859b2911a1cb7c09a292ed8
Casey Cabrales <caseycabrales@gmail.com>
2016-05-05 00:35:20 -0700

    Get user input to choose from results list. Untested

--- tabhero_functions.py
+++ tabhero_functions.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
+from lxml import html
+import requests
 
 # Class that will act as an individual tab result
 class TabResult():
-	def __init__(self, artist, title, tab_id, rating, url):
+	def __init__(self, artist, title, tab_id, rating, tab_type, url):
 		self.artist = artist
 		self.title = title
 		self.tab_id = tab_id
 		self.rating = rating
+		self.type = tab_type
 		self.url = url
 
 	# Formats the tab output for printing to terminal or writing to file
@@ -29,7 +32,27 @@ def tabs_search(query):
 	results = []
 
 	ultimate_guitar = 'https://www.ultimate-guitar.com'
+	endpoint = ultimate_guitar + '/search.php?search_type=title&order=&value=' + query
+
+	page = requests.get(endpoint)
+	tree = html.fromstring(page.content)
+
 	return results
 
-def choose_from_resuls(results):
-	print(results)
+def get_tab_from_url(url):
+	return None
+
+def choose_from_results(results):
+	for i in range(len(results)):
+		curr_result = results[i]
+		result_text = '[{}] {} - {} - {}/5 - {}'.format(i + 1, curr_result.artist, curr_result.title, curr_result.rating, curr_result.type)
+		print(result_text)
+
+	choice = 0
+	while choice < 1 or choice > len(results)
+		try:
+			choice = int(input('\nSelect a tab number: '))
+		except ValueError:
+			print('Please enter a valid number!')
+
+	return results[choice - 1]
