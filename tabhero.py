df0dfcdf0ec107d23d2a352ad3d25f4633134716
Casey Cabrales <caseycabrales@gmail.com>
2016-05-04 20:59:40 -0700

    Added main skeleton

--- tabhero.py
+++ tabhero.py
@@ -4,11 +4,11 @@ import sys
 import argparse
 from tabhero_functions import *
 
-parser = argparse.ArgumentParser(description="View and download guitar tabs from ultimate-guitar.com")
+parser = argparse.ArgumentParser(description="View and download guitar tabs from ultimate-guitar.com to the current directory")
 parser.add_argument('query', metavar='query', nargs='+',
 					help='Query song or artist to search on ultimate-guitar.com')
 #maybe remove this in future
-parser.add_argument('-p', '--print',
+parser.add_argument('-p', '--print_tab',
 					action='store_true', default=False,
 					help='Print the resulting tab to console instead of downloading directly.')
 parser.add_argument('-s', '--select',
@@ -21,3 +21,26 @@ if len(sys.argv) < 2:
 
 args = parser.parse_args()
 
+
+if __name__ == '__main__':
+	query = ' '.join(args.query)
+	search_results = tabs_search(query)
+
+	# Allow the user to choose from the results, or pick the first result by default
+	if args.select is True:
+		tab_choice = choose_from_results(search_results)
+	else:
+		tab_choice = search_results[0]
+
+	formatted_tab = tab_choice.format_tab_output()
+
+	# Print the tab to command line or download by default
+	if args.print_tab is True:
+		print ('\n' + formatted_tab,)
+	else:
+		filename = tab_choice.generate_filename()
+		with open(filename, 'w') as f:
+			try:
+				f.write(bytes(formatted_tab, 'UTF-8'))
+			except TypeError:
+				f.write(bytes(formatted_tab))
