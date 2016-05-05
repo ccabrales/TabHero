4f96e20a88f5b98ee1809c70d77289dfdb40091e
Casey Cabrales <caseycabrales@gmail.com>
2016-05-04 23:23:20 -0700

    Added helper functions. Defined class that will act as a tab search result with properties and functions.

--- tabhero_functions.py
+++ tabhero_functions.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+
+# Class that will act as an individual tab result
+class TabResult():
+	def __init__(self, artist, title, tab_id, rating, url):
+		self.artist = artist
+		self.title = title
+		self.tab_id = tab_id
+		self.rating = rating
+		self.url = url
+
+	# Formats the tab output for printing to terminal or writing to file
+	def format_tab_output(self):
+		header = '{} - {}'.format(self.artist, self.title)
+		spacer = '-' * (len(header))
+		tab = get_tab_from_url(self.url)
+
+		tab_output = '{}\n{}\n{}\n'.format(header, spacer, tab)
+		return tab_output
+
+	# Generates a file name for the tab as a text file
+	def generate_filename(self):
+		return self.artist + '-' + self.title + '-' + self.tab_id + '.txt'
+
+
+def tabs_search(query):
+	results = []
+
+	ultimate_guitar = 'https://www.ultimate-guitar.com'
+	return results
+
+def choose_from_resuls(results):
+	print(results)
